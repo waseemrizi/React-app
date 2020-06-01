65c9b48e4d7a7bbaf9769462ca136b201ead3fff
waseemrizi <waseemrizi@outlook.com>
2020-06-01 15:24:24 +0500

    chnage into app js some text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is my first editing
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          you could learn 
         </a>
       </header>
     </div>
